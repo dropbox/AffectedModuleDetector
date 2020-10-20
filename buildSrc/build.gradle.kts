fce6ea167a733b93a620bada876232355ff0bac8
Mike Nakhimovich <digitalbuddha@users.noreply.github.com>
2020-10-20 11:36:53 -0400

    Initial setup (#1)
    initial commit

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020, Dropbox, Inc. All rights reserved.
+ */
+plugins {
+    kotlin("jvm") version "1.4.10"
+    `java-gradle-plugin`
+}
+
+
+repositories {
+    google()
+    jcenter()
+}
