adb2910895cdb62189088328033a5d5670b08f24
Chris Mitchell <chrism@dropbox.com>
2020-10-21 11:45:48 -0700

    Add gradle plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,9 +3,9 @@
  */
 plugins {
     kotlin("jvm") version "1.4.10"
+    id("com.gradle.plugin-publish") version "0.12.0"
     `java-gradle-plugin`
 }
-
 
 repositories {
     google()
@@ -17,4 +17,31 @@ dependencies {
     testImplementation("com.nhaarman:mockito-kotlin:1.5.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.4.10")
     testImplementation("com.google.truth:truth:1.0.1")
+}
+
+gradlePlugin {
+    plugins {
+        create("affectedModuleDetectorPlugin") {
+            id = "com.dropbox.detector"
+            implementationClass = "com.dropbox.detector.AffectedModuleDetectorPlugin"
+        }
+    }
+}
+
+version = "0.1.0-SNAPSHOT"
+
+pluginBundle {
+    website = "https://github.com/Dropbox/AffectedModuleDetector"
+    vcsUrl = "https://github.com/Dropbox/AffectedModuleDetector"
+
+    description = "A Gradle Plugin to determine which modules were affected in a commit or branch."
+
+    (plugins) {
+
+        "affectedModuleDetectorPlugin" {
+            // id is captured from java-gradle-plugin configuration
+            displayName = "Affected Module Detector"
+            tags = listOf("module", "git", "detector", "dependency")
+        }
+    }
 }
