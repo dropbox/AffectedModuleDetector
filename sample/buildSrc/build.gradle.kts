ee1c5ce03d2eb34418bcbac04fb5e1a0d846e0b8
miken <miken@dropbox.com>
2020-11-11 15:00:41 -0500

    foo

--- sample/buildSrc/build.gradle.kts
+++ sample/buildSrc/build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.dropbox.affectedmoduledetector:affectedmoduledetector:0.1.0-SNAPSHOT")
+    implementation("com.dropbox.affectedmoduledetector:affectedmoduledetector:0.1.0")
     testImplementation("junit:junit:4.13.1")
     testImplementation("com.nhaarman:mockito-kotlin:1.5.0")
     testImplementation("com.google.truth:truth:1.0.1")
