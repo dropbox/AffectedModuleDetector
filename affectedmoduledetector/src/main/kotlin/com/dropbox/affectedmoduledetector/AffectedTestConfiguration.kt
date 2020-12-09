576d063f06e589346eb67821df54d976b19ba317
Chris Mitchell <chrism@dropbox.com>
2020-12-09 09:18:49 -0800

    Allow modules to configure the task to run

--- affectedmoduledetector/src/main/kotlin/com/dropbox/affectedmoduledetector/AffectedTestConfiguration.kt
+++ affectedmoduledetector/src/main/kotlin/com/dropbox/affectedmoduledetector/AffectedTestConfiguration.kt
@@ -3,32 +3,14 @@ package com.dropbox.affectedmoduledetector
 /**
  * Used to configure which variant to run for affected tasks by adding following block to modules
  * affectedTestConfiguration{
- *  variantToTest = "debug"
- *  jvmTestBackup = "test"
+ *  assembleAndroidTestTask = "assembleDevDebugAndroidTest"
  * }
  */
 open class AffectedTestConfiguration {
 
-    /**
-     *  Sets variant for all affected tasks
-     *  By default `run` tasks will use debug as the variant to test ie
-     *  gradlew runAffectedUnitTests will run testDebugUnitTest
-     *
-     */
-    var variantToTest: String? = null
-        get() {
-            return field ?: "debug"
-        }
-
-    /**
-     * when [jvmTest] task is not found we will try to run [jvmTestBackup]
-     * this is normally used for modules that are not android variant aware
-     */
-    var jvmTestBackup = "test"
-
-    val assembleAndroidTestTask get() = "assemble${variantToTest?.capitalize()}AndroidTest"
-    val runAndroidTestTask get() = "connected${variantToTest?.capitalize()}AndroidTest"
-    val jvmTest get() = "test${variantToTest?.capitalize()}UnitTest"
+    var assembleAndroidTestTask = "assembleDebugAndroidTest"
+    var runAndroidTestTask  = "connectedDebugAndroidTest"
+    var jvmTest = "testDebugUnitTest"
 
     companion object {
         const val name = "affectedTestConfiguration"
