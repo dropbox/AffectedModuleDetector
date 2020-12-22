2a101d57e8d61754f4fb850c94eba51caa06561c
Jonny Caley <jonathan.caley@ocado.com>
2020-12-22 18:41:39 +0000

    Fix tests to include commitShaProvider

--- affectedmoduledetector/src/test/kotlin/com/dropbox/affectedmoduledetector/GitClientImplTest.kt
+++ affectedmoduledetector/src/test/kotlin/com/dropbox/affectedmoduledetector/GitClientImplTest.kt
@@ -7,6 +7,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import com.dropbox.affectedmoduledetector.GitClientImpl.Companion.CHANGED_FILES_CMD_PREFIX
+import com.dropbox.affectedmoduledetector.mocks.MockCommandRunner
+import com.dropbox.affectedmoduledetector.mocks.MockCommitShaProvider
 
 @RunWith(JUnit4::class)
 class GitClientImplTest {
@@ -20,75 +22,59 @@ class GitClientImplTest {
      * directory somewhere in the parent directory tree.  @see [GitClientImpl]
      */
     private val workingDir = File(System.getProperty("user.dir")).parentFile
+    private val commitShaProvider = MockCommitShaProvider()
     private val client = GitClientImpl(
         workingDir = workingDir,
         logger = logger,
-        commandRunner = commandRunner
+        commandRunner = commandRunner,
+        commitShaProvider = commitShaProvider
     )
 
-
     @Test
-    fun findChangesSince() {
-        var changes = listOf(
+    fun givenChangedFiles_whenFindChangedFilesIncludeUncommitted_thenReturnChanges() {
+        val changes = listOf(
                 convertToFilePath("a", "b", "c.java"),
                 convertToFilePath("d", "e", "f.java"))
         commandRunner.addReply(
                 "$CHANGED_FILES_CMD_PREFIX HEAD..mySha",
                 changes.joinToString(System.lineSeparator())
         )
+        commitShaProvider.addReply("mySha")
+
         assertEquals(
                 changes,
-                client.findChangedFilesSince(sha = "mySha", includeUncommitted = true))
+                client.findChangedFiles(includeUncommitted = true))
     }
 
     @Test
     fun findChangesSince_empty() {
+        commitShaProvider.addReply("mySha")
         assertEquals(
                 emptyList<String>(),
-                client.findChangedFilesSince("foo"))
+                client.findChangedFiles()
+        )
     }
 
     @Test
     fun findChangesSince_twoCls() {
-        var changes = listOf(
+        val changes = listOf(
                 convertToFilePath("a", "b", "c.java"),
                 convertToFilePath("d", "e", "f.java"))
         commandRunner.addReply(
                 "$CHANGED_FILES_CMD_PREFIX otherSha mySha",
                 changes.joinToString(System.lineSeparator())
         )
+        commitShaProvider.addReply("mySha")
         assertEquals(
                 changes,
-                client.findChangedFilesSince(
-                        sha = "mySha",
-                        top = "otherSha",
-                        includeUncommitted = false))
+                client.findChangedFiles(
+                    top = "otherSha",
+                    includeUncommitted = false
+                ))
     }
 
     // For both Linux/Windows
     fun convertToFilePath(vararg list: String): String {
         return list.toList().joinToString(File.separator)
-    }
-
-    private class MockCommandRunner(val logger: ToStringLogger) : GitClient.CommandRunner {
-        private val replies = mutableMapOf<String, List<String>>()
-
-        fun addReply(command: String, response: String) {
-            logger.info("add reply. cmd: $command response: $response")
-            replies[command] = response.split(System.lineSeparator())
-        }
-
-        override fun execute(command: String): String {
-            return replies.getOrDefault(command, emptyList())
-                .joinToString(System.lineSeparator()).also {
-                    logger.info("cmd: $command response: $it")
-            }
-        }
-
-        override fun executeAndParse(command: String): List<String> {
-            return replies.getOrDefault(command, emptyList()).also {
-                logger.info("cmd: $command response: $it")
-            }
-        }
     }
 }
