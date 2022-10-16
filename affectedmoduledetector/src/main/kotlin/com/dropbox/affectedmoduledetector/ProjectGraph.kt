bb5935798350e939c5b3c45bb6883e497a82cf43
Mez Pahlan <mez.pahlan@gmail.com>
2022-10-16 17:44:16 +0100

    Fix missing import

--- affectedmoduledetector/src/main/kotlin/com/dropbox/affectedmoduledetector/ProjectGraph.kt
+++ affectedmoduledetector/src/main/kotlin/com/dropbox/affectedmoduledetector/ProjectGraph.kt
@@ -20,6 +20,7 @@
 
 package com.dropbox.affectedmoduledetector
 
+import com.dropbox.affectedmoduledetector.util.toPathSections
 import org.gradle.api.Project
 import org.gradle.api.logging.Logger
 import java.io.File
