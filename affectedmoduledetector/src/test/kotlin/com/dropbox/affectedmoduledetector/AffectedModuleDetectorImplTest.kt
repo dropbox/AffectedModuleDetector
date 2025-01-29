cef5e51bdf73cd1312b8c5ffc3bdfd7f95e4fe62
Josh Feinberg <15068619+joshafeinberg@users.noreply.github.com>
2025-01-29 14:08:01 -0600

    Fix test failing (#281)

--- affectedmoduledetector/src/test/kotlin/com/dropbox/affectedmoduledetector/AffectedModuleDetectorImplTest.kt
+++ affectedmoduledetector/src/test/kotlin/com/dropbox/affectedmoduledetector/AffectedModuleDetectorImplTest.kt
@@ -355,7 +355,7 @@ class AffectedModuleDetectorImplTest {
         MatcherAssert.assertThat(
             detector.affectedProjects,
             CoreMatchers.`is`(
-                emptySet()
+                emptyMap()
             )
         )
     }
