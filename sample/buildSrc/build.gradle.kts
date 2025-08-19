2da282990bbd7bb8cc4a76962752499ff412a4ef
Josh Feinberg <15068619+joshafeinberg@users.noreply.github.com>
2025-08-19 11:21:15 -0500

    Prepare next development version.

--- sample/buildSrc/build.gradle.kts
+++ sample/buildSrc/build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.dropbox.affectedmoduledetector:affectedmoduledetector:0.6.1-SNAPSHOT")
+    implementation("com.dropbox.affectedmoduledetector:affectedmoduledetector:0.6.2-SNAPSHOT")
     testImplementation("junit:junit:4.13.1")
     testImplementation("com.nhaarman:mockito-kotlin:1.5.0")
     testImplementation("com.google.truth:truth:1.0.1")
